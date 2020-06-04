ababbbb427405e8f233f9d6f6fe53e118494b3e8
Saket <21242528+khajuriasaket486@users.noreply.github.com>
2020-06-04 19:58:55 +0530

    ---Not Important---

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -151,8 +151,8 @@ MESSAGE_TAGS = {
 #
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'khajuria.saket486@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Twinparadox500'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
 
 

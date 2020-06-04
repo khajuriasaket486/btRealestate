6ba500158349b309a9b740c60402faba03f37115
Saket <khajuriasaket486>
2020-06-04 18:04:45 +0530

    local settings added

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -154,3 +154,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'khajuria.saket486@gmail.com'
 EMAIL_HOST_PASSWORD = 'Twinparadox500'
 EMAIL_USE_TLS = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
